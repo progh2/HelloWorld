7ed769bf1d05ec4a7c52b86ebcc9d42f8db684b4
Gihun Ham <progh2@gmail.com>
2013-01-22 00:32:10 +0900

    연습용 헬로월드 자바 소스 추가

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * GitHub 연습용 자바 예제
+ * 자신의 이름을 남기세요!!!
+ * @author progh2
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args 사용안함
+	 */
+	public static void main(String[] args) {
+		System.out.println("2013년 01월 22일에 함기훈님이 말합니다. 헬로우 월드?");	// 함기훈 인사 추가
+		
+		// TODO: 여러분의 인사 소스 코드를 아래 행에 순서대로 추가해 보세요. 
+		// System.out.println("2013년 01월 22일에 이미림님이 말합니다. NiHaoMa?");
+		
+
+	}
+
+}
